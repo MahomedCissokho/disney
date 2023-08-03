6f35f5afd605994c95b50a2f3143e64e3826db06
Mahomed Cissokho <mahomedcissokho@esp.sn>
2023-08-02 22:29:22 -0400

    adding navigation bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,51 @@
+import logo from '../assets/images/logo.png'
+import 
+{ 
+  HiHome,HiMagnifyingGlass, HiStar,
+  HiPlayCircle, HiTv
+} 
+from 'react-icons/hi2'
+import {HiPlus} from 'react-icons/hi'
+import HeaderItem from './HeaderItem'
+import profile from '../assets/images/hacker.png'
 
 function Header() {
+  const menu = [
+    {
+      name:'HOME',
+      icon: HiHome
+    },
+    {
+      name:'SEARCH',
+      icon: HiMagnifyingGlass
+    },
+    {
+      name:'WATCH LIST',
+      icon: HiPlus
+    },
+    {
+      name:'ORIGINALS',
+      icon: HiStar
+    },
+    {
+      name:'MOVIES',
+      icon: HiPlayCircle
+    },
+    {
+      name:'SERIES ',
+      icon: HiTv
+    },
+    
+  ]
   return (
-    <div>
-      <h2 className="font-bold underline bg-red-600">Hello, my name is  Mahomed Cissokho</h2>
+    <div className='flex items-center justify-between p-5'>
+      <div className='flex items-center justify-center gap-10'>
+        <img src={logo} alt='logo' className='w-[80px] md:w-[115px] object-cover'/>
+        {
+          menu.map(item => <HeaderItem key={item.id} name={item.name} Icon={item.icon}/>)
+        }
+      </div>
+      <img src={profile} alt='profile' className='w-[40px] rounded-full cursor-pointer shadow-md' />
     </div>
   )
 }
---
+++ src/components/HeaderItem.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+
+function HeaderItem({name, Icon}) {
+  return (
+    <div className="text-white flex items-center gap-5 text-[15px] cursor-pointer font-semibold hover:underline hover:underline-offset-8 transition-all duration-300 ">
+      <Icon/>
+      <h2>{name}</h2>
+    </div>
+  );
+}
+
+export default HeaderItem;
