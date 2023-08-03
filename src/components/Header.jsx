0352ae32cece631bcbf777d5d4fef566b3b939dd
Mahomed Cissokho <mahomedcissokho@esp.sn>
2023-08-02 20:01:05 -0400

    initiliaze project folder and adding components

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+function Header() {
+  return (
+    <div>
+      <h2 className="font-bold underline bg-red-600">Hello, my name is  Mahomed Cissokho</h2>
+    </div>
+  )
+}
+
+export default Header;
