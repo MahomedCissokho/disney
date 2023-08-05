71d3866b28b433a092710467fd97e459e25f6314
Mahomed Cissokho <mahomedcissokho@esp.sn>
2023-08-05 06:53:04 -0400

    finishing Slider component

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,7 +1,14 @@
 import axios from 'axios'
 
-const movieBaseUrl = 'https://api.themoviedb.org/3'
-const api_key      = '5a311d11eccff3a10de49f735aed80ee'
+const options = {
+  method: 'GET',
+  url: 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YTMxMWQxMWVjY2ZmM2ExMGRlNDlmNzM1YWVkODBlZSIsInN1YiI6IjY0OGM3MDM5NDJiZjAxMDBhZTMwODVlMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.obsyyHrwO4NQyEtMIj2H1WVGU7MOy_-Kmgkwwir1MaM'
+  }
+};
 
-export const getTrendingVideos = axios.get(movieBaseUrl+'/trending/all/day?api_key'+api_key)
-
+const getTrendingVideos = axios.request(options)
+ 
+export default {getTrendingVideos}
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,9 +1,49 @@
+import { useEffect } from "react";
+import GlobalApi from "../Services/GlobalApi";
+import { useState } from "react";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
+import { useRef } from "react";
 
+const image_base_url = "https://image.tmdb.org/t/p/original"
 function Slider() {
+
+  const [movieLists, setMovieLists] = useState([])
+  const elementRef = useRef() 
+  const currentWidth = window.innerWidth
+
+  const getMoviesTrending = () => {
+    GlobalApi.getTrendingVideos
+    .then((response) => {
+        setMovieLists(response.data.results)
+    })
+    
+  }
+
+  const sliderLeft = (element) => {
+    element.scrollLeft -=  currentWidth - 100
+  }
+
+  const sliderRight = (element) => element.scrollLeft += currentWidth - 100
+
+
+  useEffect(() => {
+    getMoviesTrending()
+
+  },[])
+
   return (
     <div>
-      Slider
+      <HiChevronLeft className="text-white text-3xl absolute mx-8 mt-40 hidden md:block cursor-pointer" onClick={() => sliderLeft(elementRef.current)}/>
+      <HiChevronRight  className="text-white text-3xl absolute mx-8 mt-40 right-0 hidden md:block cursor-pointer" onClick={() => sliderRight(elementRef.current)}/>
+      <div className="flex overflow-x-auto px-16 py-5 w-full scrollbar-none scroll-smooth transition-all duration-300"  ref={elementRef}>
+        {
+          movieLists.map((movie, index) => (
+            <img src={image_base_url+movie.backdrop_path} key={index} className="min-w-full md:h-[350px] object-cover mr-7 bg-no-repeat object-left-top hover:border-2 border-gray-100 hover:cursor-move transition-all duration-300 ease-in-out"/>
+          ))
+        }
+      </div>
     </div>
+
   );
 }
 
